a2fc50343ac6b3fd332d3eddb6c1176db49eec7c
Steve McCoy <mccoyst@gmail.com>
2017-02-11 18:41:32 -0500

    Try no SDL_Renderer

---
+++ shaders.cxx
@@ -0,0 +1,67 @@
+#include <cstdlib>
+#include <iostream>
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+
+static void check_status(GLuint);
+
+GLint init_shaders(){
+	const GLchar* vsrc =
+		"#version 150\n"
+		"in vec2 position;"
+		"uniform vec2 offset;"
+		"void main() {"
+		"	gl_Position = vec4(position+offset, 0.0, 1.0);"
+		"}";
+	const GLchar* fsrc =
+		"#version 150\n"
+		"out vec4 outColor;"
+		"void main() {"
+		"	outColor = vec4(1.0, 1.0, 1.0, 1.0);"
+		"}";
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vsrc, NULL);
+	glCompileShader(vs);
+	check_status(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fsrc, NULL);
+	glCompileShader(fs);
+	check_status(fs);
+
+	GLuint p = glCreateProgram();
+	glAttachShader(p, vs);
+	glAttachShader(p, fs);
+	glBindFragDataLocation(p, 0, "outColor");
+	glLinkProgram(p);
+	GLint ok = 0;
+	glGetProgramiv(p, GL_LINK_STATUS, &ok);
+	if(ok){
+		glUseProgram(p);
+		return p;
+	}
+
+	GLint n = 0;
+	glGetProgramiv(p, GL_INFO_LOG_LENGTH, &n);
+	char *msg = new char[n+1];
+	glGetProgramInfoLog(p, n, nullptr, msg);
+	std::cerr << "program: " << msg << '\n';
+	exit(1);
+	return 0;
+}
+
+static void check_status(GLuint s){
+	GLint ok = 0;
+	glGetShaderiv(s, GL_COMPILE_STATUS, &ok);
+	if(ok){
+		return;
+	}
+
+	GLint n = 0;
+	glGetShaderiv(s, GL_INFO_LOG_LENGTH, &n);
+	char *msg = new char[n+1];
+	glGetShaderInfoLog(s, n, nullptr, msg);
+	std::cerr << "shader: " << msg << '\n';
+	exit(1);
+}
--- square.cxx
+++ square.cxx
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <GL/glew.h>
 #include <SDL2/SDL.h>
 
-struct Ball {
-	SDL_Rect r;
-	int dx, dy;
-
-	void move();
-	void draw(SDL_Renderer *rend, float draw_interp);
-};
+GLint init_shaders();
 
 int width = 640, height = 480;
 
@@ -18,34 +13,82 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	auto win = SDL_CreateWindow("Prong",
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	if(SDL_GL_SetSwapInterval(-1) < 0){
+		// Late-swap tearing isn't possible, so use good old vsync:
+		SDL_GL_SetSwapInterval(1);
+	}
+
+	auto win = SDL_CreateWindow("Square",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		width,
 		height,
-		SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE|SDL_WINDOW_ALLOW_HIGHDPI);
+		SDL_WINDOW_OPENGL|SDL_WINDOW_ALLOW_HIGHDPI);
 	if(!win){
 		std::cerr << "SDL_CreateWindow: " << SDL_GetError() << '\n';
 		return 1;
 	}
 
-	auto rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-	SDL_RenderSetLogicalSize(rend, width, height);
+	SDL_GL_CreateContext(win);
+	glewExperimental = GL_TRUE; 
+	auto err = glewInit();
+	if(err != GLEW_OK){
+		std::cerr << "glewinit: " << glewGetErrorString(err) << '\n';
+		return 1;
+	}
 
-	Ball ball = {
-		{(width-8-64)/2, (height-8-64)/2, 32, 32},
-		-1, -1,
+	std::cerr << glGetString(GL_VERSION) << '\n';
+	std::cerr << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
+	for(;;){
+		auto err = glGetError();
+		if(err == GL_NO_ERROR){
+			break;
+		}
+	}
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	GLfloat verts[] = {
+		-0.04f, 0.04f,
+		0.04f, 0.04f,
+		0.04f, -0.04f,
+		0.04f, -0.04f,
+		-0.04f, 0.04f,
+		-0.04f, -0.04f,
 	};
 
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	auto p = init_shaders();
+	GLint pos = glGetAttribLocation(p, "position");
+	glEnableVertexAttribArray(pos);
+	glVertexAttribPointer(pos, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	GLint off = glGetUniformLocation(p, "offset");
+
 	Uint32 last_time = 0;
-	Uint32 frame_ms = 8, lag = 0;
+	//Uint32 frame_ms = 16, lag = 0;
+	GLfloat dx = 1/float(width);
 
 	last_time = SDL_GetTicks();
 	for(;;){
-		auto current_time = SDL_GetTicks();
-		auto dt = current_time - last_time;
-		last_time = current_time;
-		lag += dt;
+		//auto current_time = SDL_GetTicks();
+		//auto dt = current_time - last_time;
+		//last_time = current_time;
+		//lag += dt;
 
 		SDL_Event e;
 		while(SDL_PollEvent(&e)){
@@ -54,44 +97,21 @@ int main(int argc, char *argv[]){
 			}
 		}
 
-		while(lag >= frame_ms){
-			ball.move();
-			lag -= frame_ms;
-		}
-		auto draw_interp = float(lag) / float(frame_ms);
+		//while(lag >= frame_ms){
+		//	lag -= frame_ms;
+		//}
+		//auto draw_interp = float(lag) / float(frame_ms);
 
-		SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
-		SDL_RenderClear(rend);
-		ball.draw(rend, draw_interp);
-		SDL_RenderPresent(rend);
+		glClearColor(0,0,0,1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUniform2f(off, dx, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		SDL_GL_SwapWindow(win);
+		dx += 3/float(width);
+		if(dx > 1.0){
+			dx = -1;
+		}
 	}
 
 	return 0;
-}
-
-void Ball::draw(SDL_Renderer *rend, float draw_interp) {
-	auto interp_ball = this->r;
-	if(draw_interp != 0){
-		interp_ball.x += draw_interp*this->dx;
-		interp_ball.y += draw_interp*this->dy;
-	}
-	SDL_SetRenderDrawColor(rend, 64, 128, 255, 255);
-	SDL_RenderFillRect(rend, &interp_ball);
-}
-
-void Ball::move() {
-	this->r.x += this->dx;
-	this->r.y += this->dy;
-	if(this->r.x < -this->r.w){
-		this->r.x = width;
-	}
-	if(this->r.x > width){
-		this->r.x = 0;
-	}
-	if(this->r.y < 0){
-		this->dy = -this->dy;
-	}
-	if(this->r.y > height-this->r.h){
-		this->dy = -this->dy;
-	}
 }
